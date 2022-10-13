f16d7cb5efa92822ed50e9ae1651643d72b00826
maiza <maiza@maizas-MacBook-Air.local>
2022-10-13 18:08:48 +0100

    first commit

---
+++ refactor_exercise.py
@@ -0,0 +1,82 @@
+# age = int(input("enter age"))
+# def movie_rating(age):
+#     if age > 117:
+#      print("age must be lower than 117")
+#     elif age >= 18:
+#      print("allowed to watch all movies")
+#     elif 18< age <= 16:
+#      print("allowed movies starting from 16")
+#     elif 16< age <= 12:
+#      print("not allowed to watch 16 plus movies")
+#     elif age < 12:
+#      print("parental guidance is required")
+#     else:
+#      print("please enter your age again")
+#     return f"your age is {age}"
+# print(movie_rating(age))
+
+
+
+
+
+# age = int(input("what is your age: "))
+# def year_born(age):
+#             current_year = 2022
+#             birth_year = current_year - age
+#             return f"you were born in  {birth_year}"
+# print(year_born(age))
+
+
+
+
+
+
+
+# item = input('Enter item to order: ')
+# def customer(item):
+#     menu = ["Chicken curry", "Vegetable curry", "Lamb curry", "Chips", "Chicken Wrap", "Veggie Wrap",
+#         "Dr Pepper", "Fanta", "Coke", "Water"]
+#
+#     order_length = 3
+#     new_order = []
+#
+#     for i in menu:
+#         print(i)
+#
+#     for food in range(order_length):
+#         item = input("add another item: ")
+#         new_order.append(item)
+#     return f"your order is {new_order}"
+#
+#     print(new_order)
+# print(customer(item))
+
+
+
+
+# def num(range):
+#         for num in range(1,101):
+#
+#           if(num%3==0 and num%5==0):
+#             print("FizzBuzz")
+#           elif(num%3 == 0):
+#             print("Fizz")
+#           elif(num%5 == 0):
+#             print("Buzz")
+#           else:
+#             print(num)
+#         return f"for every number divisible by 3 it returns fizz, and for 5 buzz and for both fizzbuzz"
+# print(num(range))
+
+
+
+
+
+
+
+
+
+
+
+
+
