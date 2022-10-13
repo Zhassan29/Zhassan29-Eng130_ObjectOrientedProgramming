5c8ecce2399aecd7ae9c5b4802fd9cc0f6596080
maiza <maiza@maizas-MacBook-Air.local>
2022-10-13 14:48:00 +0100

    first commit

---
+++ exercise_1.py
@@ -0,0 +1,14 @@
+def multiply(value1, value2): # take user input as int then  multiply them together display the outcome
+    print("this function is to provide multiplication functionality")
+    return value1 * value2
+print(multiply(5, 5))
+
+def modulus(value1, value2): # take user input as int then together display the outcome
+    print("this function is to provide the remainder")
+    return value1 % value2
+print(modulus(12, 5))
+
+def divide(value1, value2): # take user input as int then  divide them and display the outcome
+    print("this function is to provide division functionality")
+    return value1 / value2
+print(divide(100, 5))
