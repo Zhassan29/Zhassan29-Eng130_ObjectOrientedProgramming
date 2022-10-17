3c030099fcd46b3d1382bcec5aff1779917ab205
maiza <maiza@maizas-MacBook-Air.local>
2022-10-17 10:50:59 +0100

    first commit

--- exam_prac.py
+++ exam_prac.py
@@ -14,13 +14,39 @@
 #  for & while loops
 # - how to iterate through a list of 5 items
 
-food = ["choc", "crisp", "sweets", "drinks"]
-for x in food:
-    print(x)
-i = 0
-while i < len(food):
-    print(food[i])
-    i = i + 1
+# food = ["choc", "crisp", "sweets", "drinks"]
+# for x in food:
+#     print(x)
+# i = 0
+# while i < len(food):
+#     print(food[i])
+#     i = i + 1
+
+
+
+# Data collections - dictionary - lists - tuple - indexing
+# - print 3rd and 5th item from list - tuple - or dictionary
+# - nested dict - list inside a dict
+# - the list item from dict
+# - which data type is mutable - immutable -
+
+# food = ["choc", "crisp", "sweets", "drinks", "nuts"]
+# print(food[0])
+# print(food[4])
+dict = {
+    "alex": 5,
+    "ben": 10,
+    "carly": 12,
+    "dan": 7
+}
+
+dict["food"] = ["choc", "crisp", "sweets", "drinks", "nuts"]
+print(dict)
+
+
+
+
+
 
 
 
